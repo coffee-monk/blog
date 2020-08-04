13936d9e326e80d8454ce3c6d16cb6e7a7652978
coffee-monk <coffeemonk@hotmail.com>
2020-08-04 18:31:53 -0400

    removed empty component

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/src/pages/404.js"))),
-  "component---src-pages-blog-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/src/pages/blog.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/src/pages/index.js"))),
-  "component---src-pages-portfolio-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/src/pages/portfolio.js"))),
-  "component---src-templates-blog-post-js": hot(preferDefault(require("/home/acorn/Desktop/personal-blog/src/templates/blog-post.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/acorn/Desktop/blog/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/acorn/Desktop/blog/src/pages/404.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/home/acorn/Desktop/blog/src/pages/blog.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/home/acorn/Desktop/blog/src/pages/index.js"))),
+  "component---src-pages-portfolio-js": hot(preferDefault(require("/home/acorn/Desktop/blog/src/pages/portfolio.js"))),
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/home/acorn/Desktop/blog/src/templates/blog-post.js")))
 }
 
--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b5945c1aaef21bdff838";
+/******/ 	var hotCurrentHash = "a12481d47b1a7afb464f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -933,8 +933,8 @@ setImmediate(function () {
     return;
   }
 
-  reactHotLoader.register(_netlifyIdentityWidget, "_netlifyIdentityWidget", "/home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
-  reactHotLoader.register(addLoginListener, "addLoginListener", "/home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
+  reactHotLoader.register(_netlifyIdentityWidget, "_netlifyIdentityWidget", "/home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
+  reactHotLoader.register(addLoginListener, "addLoginListener", "/home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
 })();
 
 ;
@@ -1005,8 +1005,8 @@ _netlifyCmsApp.default.registerPreviewStyle("cms.css");
     return;
   }
 
-  reactHotLoader.register(_netlifyCmsApp, "_netlifyCmsApp", "/home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms.js");
-  reactHotLoader.register(_emitter, "_emitter", "/home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms.js");
+  reactHotLoader.register(_netlifyCmsApp, "_netlifyCmsApp", "/home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms.js");
+  reactHotLoader.register(_emitter, "_emitter", "/home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms.js");
 })();
 
 ;
@@ -1653,8 +1653,8 @@ module.exports = function(module) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms.js */"./node_modules/gatsby-plugin-netlify-cms/cms.js");
-module.exports = __webpack_require__(/*! /home/acorn/Desktop/personal-blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js */"./node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
+__webpack_require__(/*! /home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms.js */"./node_modules/gatsby-plugin-netlify-cms/cms.js");
+module.exports = __webpack_require__(/*! /home/acorn/Desktop/blog/node_modules/gatsby-plugin-netlify-cms/cms-identity.js */"./node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
 
 
 /***/ }),
