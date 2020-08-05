eb82bcdc6b2080f4f6d1d3018de798375bc7245b
coffee-monk <coffeemonk@hotmail.com>
2020-08-05 10:16:53 -0400

    change typography to bootstrap

---
+++ .cache/caches/gatsby-plugin-typography/typography.js
@@ -0,0 +1,1 @@
+module.exports = require("/home/acorn/Desktop/blog/src/utils/typography")
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,22 +1,18 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import bootstrapTheme from "typography-theme-bootstrap"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+bootstrapTheme.overrideThemeStyles = () => {
+	return {
+		"a": {
+			boxShadow: `none`,
+			textDecoration: `none`,
+			},
+}}
 
-delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
 
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
+
+const typography = new Typography(bootstrapTheme)
 
 export default typography
 export const rhythm = typography.rhythm
