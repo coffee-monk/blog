399da47077db3e3759ca6e0cac6260ff4c67f9ec
coffee-monk <coffeemonk@hotmail.com>
2020-08-05 09:26:47 -0400

    change to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,11 +82,11 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-typography`,
-    //   options: {
-    //     pathToConfigModule: `src/utils/typography`,
-    //   },
-    // },
+     {
+       resolve: `gatsby-plugin-typography`,
+       options: {
+         pathToConfigModule: `src/utils/typography`,
+       },
+     },
   ],
 }
